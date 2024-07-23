a7d14e0b5928fad0fa9298ea62dde880ac5c0578
Bro3131 <Kutishev2005@mail.ru>
2024-07-23 23:51:20 +0600

    added Death screen menu

--- ui.gd
+++ ui.gd
@@ -3,9 +3,14 @@ extends CanvasLayer
 @onready var main_menu = $MainMenu
 @onready var settings_menu = $SettingsMenu
 @onready var ui = $"."
-@onready var node = $".."
+@onready var end_game_screen = $EndGameScreen
 
-
+func _ready():
+	end_game_screen.visible = false 
+	main_menu.visible = true
+	settings_menu.visible = false
+	get_tree().paused = false
+	pass
 
 #main Menu
 func _on_start_pressed():
@@ -25,3 +30,15 @@ func _on_quit_pressed():
 func _on_back_pressed():
 	main_menu.visible = true
 	settings_menu.visible = false
+
+
+# Death screen 
+
+func _on_restart_pressed():
+	end_game_screen.visible = false
+	get_tree().change_scene_to_file("res://testgame.tscn")
+	
+
+
+func _on_go_back_pressed():
+	get_tree().change_scene_to_file("res://ui.tscn")
